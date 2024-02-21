65bef3b0975fa8111385441e2234dce80d7c952d
ricohaiti <taneon98@gmail.com>
2024-02-21 20:09:10 +0000

    dealerrr

--- [job_ill]/GoatRico_Dealer_V2/shared/webhook.lua
+++
@@ -1,6 +0,0 @@
-Webhook = {}
-
--- Discord webhook links here for logs
-Webhook.Links = {
-    drugsale = 'https://discord.com/api/webhooks/1183456741132861501/E7T1Dvib1GeB95N7aPvejfcoOsqg6SUkpUwz0bPoISQHCaQ7n_Yr70fORDbpXsTaan3W',
-}
