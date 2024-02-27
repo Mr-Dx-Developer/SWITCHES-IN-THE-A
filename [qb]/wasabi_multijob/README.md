bd892a36623c3814812925b8afaf2aeb20e8a7f5
Tanvir Anjum Neon <taneon98@gmail.com>
2024-02-27 09:10:34 +0600

    old multijob remove

--- [qb]/wasabi_multijob/README.md
+++
@@ -1,1 +0,0 @@
-https://wasabi-scripts.gitbook.io/wasabi-scripts/wasabi_multijob
--- [qb]/wasabi_multijob/server/sv_customize.lua
+++
@@ -1,4 +0,0 @@
------------------For support, scripts, and more----------------
---------------- https://discord.gg/wasabiscripts  -------------
----------------------------------------------------------------
-ESX = exports.es_extended:getSharedObject() -- Customize ESX ?
