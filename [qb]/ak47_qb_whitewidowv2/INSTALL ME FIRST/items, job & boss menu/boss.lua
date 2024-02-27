11859319cec0740562320b92b85eb2a081835de3
Tanvir Anjum Neon <taneon98@gmail.com>
2024-02-27 09:12:00 +0600

    white widow v2

---
+++ [qb]/ak47_qb_whitewidowv2/INSTALL ME FIRST/items, job & boss menu/boss.lua
@@ -0,0 +1,7 @@
+['cookies'] = {
+    vector3(-934.1, -1168.34, 5.11), --place this in qb-management/client/cl_config.lua [Config.BossMenus]
+},
+
+['cookies'] = {
+    {coords = vector3(-934.1, -1168.34, 5.11), length = 1.15, width = 2.6, heading = 353.0, minZ = 4.11, maxZ = 5.99}, --place this in qb-management/client/cl_config.lua [Config.BossMenuZones]
+},
---
+++ [qb]/ak47_qb_whitewidowv2/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.UseProgressBar = true							--Set your progress bar in utils.lua then enable this
+Config.SocietyInManagement = false --enable if your society is inside qb-management
+
+Config.UseTarget = false
+Config.TatgetScriptName = 'qb-target'
+Config.DebugPoly = false 
+
+Config.Marker = {
+	type = 2,
+	color = {r = 50, g = 205, b = 50, a = 200},
+	size = {x = 0.2, y = 0.2, z = 0.1},
+	font = 0,
+	fontSize = 0.4,
+}
+
