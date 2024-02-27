a6077bbda741f38f8496da1a3f5b4bbf2fb1ee0a
Tanvir Anjum Neon <taneon98@gmail.com>
2024-02-27 09:09:39 +0600

    Dispatch

--- [jobs_whit]/[dispatch]/qs-dispatch/client/custom/billing/jaksam-billing-ui.lua
+++
@@ -1,7 +0,0 @@
-if Config.Billing ~= "jaksam-billing-ui" then
-    return
-end
-
-function CreateBill(amount, text)
-    TriggerServerEvent("billing_ui:sendBill", GetPlayerServerId(PlayerId()), 'society_police', text, amount)
-end
