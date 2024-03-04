49ea23bf6e566d4f361deccfe5da6d44ae3a2cd7
Tanvir Anjum Neon <taneon98@gmail.com>
2024-03-04 20:55:32 +0600

    polyzone create

--- [standalone]/Ped_PolyZone/config.lua
+++ [standalone]/Ped_PolyZone/config.lua
@@ -1,10 +1,10 @@
 PedZones = {
     Zona1 = {
         pontos = {
-            vector2(95.779289245605,-561.05999755859), -- Coordinates "x","y"
-            vector2(115.30712127686,-567.89324951172),
-            vector2(111.94038391113,-574.27203369141),
-            vector2(97.516830444336,-570.61962890625) -- It will connect the last point to the 1st point
+            vector2(89.16923522949219,-1851.230712890625), -- Coordinates "x","y"
+            vector2(148.70770263671875,-1780.5494384765625),
+            vector2(46.5362663269043,-1695.243896484375),
+            vector2(-48.71208953857422,-1731.3494873046875) -- It will connect the last point to the 1st point
         },
         options = {
             name = 'Zona 1',
@@ -13,15 +13,82 @@ PedZones = {
     },
     Zona2 = {
         pontos = {
-            vector2(0,0), -- Coordinates "x","y"
-            vector2(0,0),
-            vector2(0,0),
-            vector2(0,0) -- It will connect the last point to the 1st point
+            vector2(-97.27912139892578,-1754.3341064453125), -- Coordinates "x","y"
+            vector2(-55.43735885620117,-1733.5252685546875),
+            vector2(-8.452743530273438,-1772.953857421875),
+            vector2(-36.9626350402832,-1808.5714111328125) -- It will connect the last point to the 1st point
         },
         options = {
             name = 'Zona 2',
             debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
         }
     },
+    Zona3 = {
+        pontos = {
+            vector2(-127.74066162109375,-1546.7340087890625), -- Coordinates "x","y"
+            vector2(-51.13846206665039,-1610.4791259765625),
+            vector2(-138.55384826660156,-1713.6658935546875),
+            vector2(-177.5340576171875,-1674.4615478515625),
+            vector2(-174.2241668701172,-1601.6043701171875) -- It will connect the last point to the 1st point
+        },
+        options = {
+            name = 'Zona 3',
+            debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
+        }
+    },
+    Zona4 = {
+        pontos = {
+            vector2(3.9164838790893555,-1605.138427734375), -- Coordinates "x","y"
+            vector2(54.382415771484375,-1646.822021484375),
+            vector2(149.24835205078125,-1592.967041015625),
+            vector2(69.78461456298828,-1523.182373046875)
+            
+        },
+        options = {
+            name = 'Zona 4',
+            debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
+        }
+    },
+    Zona5 = {
+        pontos = {
+            vector2(87.66593933105469,-1504.6021728515625), -- Coordinates "x","y"
+            vector2(174.54066467285156,-1582.5626220703125),
+            vector2(288.22418212890625,-1514.10986328125),
+            vector2(160.4571533203125,-1418.4263916015625)
+            
+        },
+        options = {
+            name = 'Zona 5',
+            debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
+        }
+    },
+    Zona6 = {
+        pontos = {
+            vector2(78.14505767822266,-1671.3494873046875), -- Coordinates "x","y"
+            vector2(271.1736145019531,-1833.046142578125),
+            vector2(348.6593322753906,-1741.4505615234375),
+            vector2(185.3142852783203,-1606.15380859375)
+            
+        },
+        options = {
+            name = 'Zona 6',
+            debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
+        }
+    },
+
+    Zona7 = {
+        pontos = {
+            vector2(210.89669799804688,-1597.107666015625), -- Coordinates "x","y"
+            vector2(314.69012451171875,-1535.5384521484375),
+            vector2(429.032958984375,-1634.6109619140625),
+            vector2(355.8725280761719,-1722.7120361328125)
+            
+        },
+        options = {
+            name = 'Zona 7',
+            debugPoly = false -- If "true" it will demarcate the area with green "walls", if "false" it will not demarcate them.
+        }
+    },
+
 
 }
