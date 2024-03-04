1fb60004b9b0c1bdcbbe2df5b45b9533aaa8b948
ricohaiti <outdamudatl@gmail.com>
2024-03-04 14:09:08 +0000

    job price

--- [qb]/qb-core/shared/jobs.lua
+++ [qb]/qb-core/shared/jobs.lua
@@ -351,11 +351,11 @@ QBShared.Jobs = {
 		defaultDuty = true,
 		offDutyPay = false,
 		grades = {
-			['0'] = { name = 'Recruit', payment = 50 },
-			['1'] = { name = 'Novice', payment = 75 },
-			['2'] = { name = 'Experienced', payment = 100 },
-			['3'] = { name = 'Advanced', payment = 125 },
-			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+			['0'] = { name = 'Recruit', payment = 250 },
+			['1'] = { name = 'Novice', payment = 375 },
+			['2'] = { name = 'Experienced', payment = 800 },
+			['3'] = { name = 'Advanced', payment = 925 },
+			['4'] = { name = 'Manager', isboss = true, payment = 1500 },
 		},
 	},
 	bigvroycustoms = {
@@ -364,11 +364,11 @@ QBShared.Jobs = {
 		defaultDuty = true,
 		offDutyPay = false,
 		grades = {
-			['0'] = { name = 'Recruit', payment = 50 },
-			['1'] = { name = 'Novice', payment = 75 },
-			['2'] = { name = 'Experienced', payment = 100 },
-			['3'] = { name = 'Advanced', payment = 125 },
-			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+			['0'] = { name = 'Recruit', payment = 250 },
+			['1'] = { name = 'Novice', payment = 375 },
+			['2'] = { name = 'Experienced', payment = 800 },
+			['3'] = { name = 'Advanced', payment = 925 },
+			['4'] = { name = 'Manager', isboss = true, payment = 1500 },
 		},
 	},
 	
