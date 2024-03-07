83378a5a77adb29b7233ccbb24dddca884a791fc
ricohaiti <outdamudatl@gmail.com>
2024-03-06 18:38:45 -0800

    side up

--- [qb]/qb-inventory/config.lua
+++ [qb]/qb-inventory/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
-Config.MaxInventoryWeight = 120000 -- Max weight a player can carry (default 120kg, written in grams)
+Config.MaxInventoryWeight = 1200000 -- Max weight a player can carry (default 120kg, written in grams)
 Config.MaxInventorySlots = 41 -- Max inventory slots for a player
 
 Config.CleanupDropTime = 15 * 60 -- How many seconds it takes for drops to be untouched before being deleted
