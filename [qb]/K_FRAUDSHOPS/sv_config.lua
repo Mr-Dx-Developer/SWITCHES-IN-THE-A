6f88d1181f8534483de06d903fed9d1c0347ca1e
ricohaiti <outdamudatl@gmail.com>
2024-03-06 18:38:30 -0800

    price change

--- [qb]/K_FRAUDSHOPS/sv_config.lua
+++ [qb]/K_FRAUDSHOPS/sv_config.lua
@@ -5,4 +5,4 @@ KonfigSV = {}
 KonfigSV.SetCheckSellPrice = false --set to false to enable random sell price or a number to have a set price
 --example KonfigSV.SetCheckSellPrice = 1000 -- $1000
 KonfigSV.CheckSellMin = 500
-KonfigSV.CheckSellMax = 2250
+KonfigSV.CheckSellMax = 1300
