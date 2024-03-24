614442fade070d26666ed7160161fcc2c7ddcf92
ricohaiti <outdamudatl@gmail.com>
2024-03-23 17:07:00 -0700

    police siren

---
+++ [unique]/Police_Siren/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+description 'FNF Police Siren'
+version '1.0'
+
+client_scripts {
+    'polcesiren.lua'
+}
+
+server_scripts {
+    'spolcesiren.lua'
+}
+
