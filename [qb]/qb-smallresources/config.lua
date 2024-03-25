ea0feb2a0e312a7bdcf67eae2a90feeb05e4200b
ricohaiti <outdamudatl@gmail.com>
2024-03-25 06:52:13 -0700

    Density

--- [qb]/qb-smallresources/config.lua
+++ [qb]/qb-smallresources/config.lua
@@ -71,11 +71,11 @@ Config.Discord = {
 }
 
 Config.Density = {
-    parked = 0.5,
-    vehicle = 0.5,
-    multiplier = 0.5,
+    parked = 0.3,
+    vehicle = 0.3,
+    multiplier = 0.3,
     peds = 0.5,
-    scenario = 0.5
+    scenario = 0.3
 }
 
 Config.Disable = {
