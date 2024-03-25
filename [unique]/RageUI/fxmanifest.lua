8ecebef71321d798ac82981a562b40e3701924c8
ricohaiti <outdamudatl@gmail.com>
2024-03-24 20:02:08 -0700

    RAGE UI ADD

---
+++ [unique]/RageUI/fxmanifest.lua
@@ -0,0 +1,18 @@
+---
+--- @author Dylan MALANDAIN, Kalyptus
+--- @version 1.0.0
+--- created at [24/05/2021 10:02]
+---
+
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+client_scripts {
+    'src/RageUI.lua',
+    'src/Menu.lua',
+    'src/MenuController.lua',
+    'src/components/*.lua',
+    'src/elements/*.lua',
+    'src/items/*.lua',
+}
