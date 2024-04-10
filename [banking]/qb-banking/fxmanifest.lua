aa221580b6a4e9786dcd683c179108dd5dde75e3
Mr_Dx <taneon98@gmail.com>
2024-04-09 22:24:54 -0700

    Bank System Update

--- [banking]/qb-banking/fxmanifest.lua
+++ [banking]/qb-banking/fxmanifest.lua
@@ -1,35 +1,41 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'
-author 'Kakarot'
-description 'Allows players to deposit/withdraw money from their bank/job account and create shared accounts'
-version '2.0.0'
+
+description 'QB-Banking'
+version '1.2.1'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
-    'config.lua'
-}
-
-client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
-    'client.lua'
+    'config/config.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server.lua'
+    'server/wrappers/business.lua',
+    'server/wrappers/useraccounts.lua',
+    'server/wrappers/gangs.lua',
+    'server/main.lua'
 }
 
-ui_page 'html/index.html'
+client_script {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/main.lua'
+}
+
+ui_page 'nui/index.html'
 
 files {
-    'html/index.html',
-    'html/script.js',
-    'html/style.css',
+    'nui/images/logo.png',
+    'nui/scripting/jquery-ui.css',
+    'nui/scripting/external/jquery/jquery.js',
+    'nui/scripting/jquery-ui.js',
+    'nui/style.css',
+    'nui/index.html',
+    'nui/qb-banking.js',
 }
+
+lua54 'yes'
