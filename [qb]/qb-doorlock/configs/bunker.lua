73783980178d055657b6b20d2e51ad73e8e2a19d
Mr_Dx <taneon98@gmail.com>
2024-04-17 19:35:45 -0700

    bunker lock

---
+++ [qb]/qb-doorlock/configs/bunker.lua
@@ -0,0 +1,15 @@
+
+
+-- bunket created by DEV Dx
+Config.DoorList['bunker-bunket'] = {
+    doorLabel = 'bunker',
+    doorRate = 1.0,
+    doors = {
+        {objName = -328548271, objYaw = 209.99810791016, objCoords = vec3(3139.772461, 5374.636230, 25.140255)},
+        {objName = 243434624, objYaw = 30.004049301147, objCoords = vec3(3144.260986, 5377.227539, 25.140255)}
+    },
+    locked = true,
+    authorizedCitizenIDs = { ['DHA78509'] = true },
+    doorType = 'doublesliding',
+    distance = 2,
+}
